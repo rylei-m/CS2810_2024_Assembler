e201144387a0df331b645d8d4b23f853cbc880f8
rylei-m <rymindrum@gmail.com>
2024-12-06 12:05:21 -0700

    Debugging S4 NYW for T4

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ from assembler import build_data_table, build_label_table, encode_program, prepr
 def main():
     # Defining the assembly file to read from
     folder = "Files"
-    filename = os.path.join(folder, "test7.asm")
+    filename = os.path.join(folder, "test9.asm")
 
     if not os.path.exists(filename):
         print(f"ERROR: The file {filename} does not exist.")
@@ -18,28 +18,34 @@ def main():
 
     # Step 1: Preprocess the lines to remove comments and whitespace
     lines = preprocess(lines)
-
+    print("STEP 1: Preprocess the lines to remove comments and whitespace")
     print("Preprocessed Lines:", lines)
+    print(" ")
 
     # Step 2: Use the preprocessed program to build data table
     data_table, data_list, processed_program = build_data_table(lines)
-
+    print("STEP 2: Use the preprocessed program to build data table")
     print("Data Table:", data_table)
     print("Data List:", data_list)
     print("Processed Program:", processed_program)
+    print(" ")
 
     # Step 3: Build a label table and strip out the labels from the code
     label_table, instructions = build_label_table(lines)
 
+    print("STEP 3: Build a label table and strip out the labels from the code")
     print("Label Table:", label_table)
     print("Processed Instructions:", instructions)
+    print(" ")
 
     # Step 4: Encode the program into a list of binary strings
     binary_instructions = encode_program(lines, label_table, data_table)
 
+    print("STEP 4: Encode the program into a list of binary strings")
     print("Binary Instructions:")
     for binary in binary_instructions:
         print(binary)
+    print(" ")
 
     # Step 5: Convert the strings to hexadecimal and write them to a file
     # hex_program = post_process(encoded_program)

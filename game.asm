9d02376649cb9c82d818830422c40d380b52df23
Rylei Mindrum <rymindrum@gmail.com>
2024-12-06 17:18:30 -0700

    simple game init -- NYW

---
+++ game.asm
@@ -0,0 +1,34 @@
+.data
+screenStart: 16384
+screenEnd: 24576
+ballColor: 65535
+backgroundColor: 0
+rowShift: 128
+xVelocity: 1
+yVelocity: 1
+ballPos: 16704
+
+.text
+j main
+
+drawBall:
+    sw R1, 0(R2)
+    jr R7
+
+clearBall:
+    sw R1, 0(R2)
+    jr R7
+
+moveBall:
+    lw R1, xVelocity
+    lw R2, ballPos
+    add R2, R2, R1
+    lw R3, rowShift
+    lw R4, yVelocity
+    bne R4, R0, updateY
+
+updateY:
+    add R2, R2, R4
+    
+checkCollision:
+    lw R3, screenStart
